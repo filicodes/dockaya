8b7cc40ecf6a551dd7aedb9ceb4bb3c122001ddc
jhiinx <nimeshadls8@gmail.com>
2025-04-28 17:34:08 +0930

    entry point added to dockaya

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,15 @@ git config --global user.email "nimeshadls8@gmail.com"
 
 echo "exec 2"
 
+git branch -M main
+echo "exec 3"
 # Set remote URL with GitHub token
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/filicodes/dockaya.git
 
-echo "exec 3"
-
-# Pull latest (optional)
-git pull origin main
 echo "exec 4"
+
+git remote -v
+
 
 # Make empty commit and push
 git commit --allow-empty -m "Container restarted at $(date)"
